371a0345e387cc1a98ffd54633f8c5da5c04831e
teeverc <72298507+teeverc@users.noreply.github.com>
2024-11-03 19:11:56 -0800

    Fix: try css approach

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
 
-import markerIcon2xUrl from "leaflet/dist/images/marker-icon-2x.png";
-import markerIconUrl from "leaflet/dist/images/marker-icon.png";
-import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
-
-// Fix Leaflet's default icon path issue
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: markerIcon2xUrl,
-	iconUrl: markerIconUrl,
-	shadowUrl: markerShadowUrl,
-});
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
