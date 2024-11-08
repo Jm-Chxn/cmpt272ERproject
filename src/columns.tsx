7e1dd4eea823e51f2c1213e45277baec76a7ad98
teeverc <72298507+teeverc@users.noreply.github.com>
2024-11-08 00:41:28 -0800

    chore: remove unused variables

--- src/columns.tsx
+++ src/columns.tsx
@@ -2,24 +2,23 @@ import { ColumnDef } from "@tanstack/react-table";
 import { EmergencyLocation, useLocations } from "./hooks/locations";
 import { useState } from "react";
 
-import { MoreHorizontal, LineChart, Trash2, ListCheck } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { MoreHorizontal, LineChart, Trash2, ListCheck } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { 
-	Drawer, 
-	DrawerContent, 
-	DrawerHeader, 
-	DrawerTitle, 
-	DrawerDescription 
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	Drawer,
+	DrawerContent,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerDescription,
 } from "@/components/ui/drawer";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 export const columns: ColumnDef<EmergencyLocation>[] = [
 	{
@@ -41,61 +40,59 @@ export const columns: ColumnDef<EmergencyLocation>[] = [
 	{
 		id: "actions",
 		cell: ({ row }) => {
-		  const location = row.original;
-		  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-		  const { markAsResolved, locations } = useLocations();
-	  
-		  return (
-			<>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="ghost" className="h-8 w-8 p-0">
-						<span className="sr-only">Open menu</span>
-						<MoreHorizontal className="h-4 w-4" />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuItem
-							onClick={() => markAsResolved(location.id)}
-						>
-							<ListCheck className="h-4 w-4" />
-							Mark as resolved</DropdownMenuItem>
-						<DropdownMenuItem
-							onClick={() => setIsDrawerOpen(true)}
-						>
-							<LineChart className="h-4 w-4" />
-							View details</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem
-							onClick={() => {/*Add delete functionality here. Does nothing for now*/}}
-						>
-							<Trash2 className="h-4 w-4" />
-							Delete</DropdownMenuItem>
+			const location = row.original;
+			const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+			const { markAsResolved } = useLocations();
 
-					</DropdownMenuContent>
-				</DropdownMenu>
-				<Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
-					<DrawerContent style={{ zIndex: 1000 }}>
-					<DrawerHeader>
-						<DrawerTitle>
-							<h2 className="text-3xl font-bold tracking-tight">
-								Incident Details
-							</h2>
-						</DrawerTitle>
-					</DrawerHeader>
-					<DrawerDescription>
-						<Card>
-							<CardContent>
-								<img src={location.pictureLink} alt="Incident Image"/>
-							</CardContent>
-						</Card>
-						
-					</DrawerDescription>
-					</DrawerContent>
-				</Drawer>
-			</>
-		  );
+			return (
+				<>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant="ghost" className="h-8 w-8 p-0">
+								<span className="sr-only">Open menu</span>
+								<MoreHorizontal className="h-4 w-4" />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							<DropdownMenuItem onClick={() => markAsResolved(location.id)}>
+								<ListCheck className="h-4 w-4" />
+								Mark as resolved
+							</DropdownMenuItem>
+							<DropdownMenuItem onClick={() => setIsDrawerOpen(true)}>
+								<LineChart className="h-4 w-4" />
+								View details
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem
+								onClick={() => {
+									/*Add delete functionality here. Does nothing for now*/
+								}}
+							>
+								<Trash2 className="h-4 w-4" />
+								Delete
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+					<Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+						<DrawerContent style={{ zIndex: 1000 }}>
+							<DrawerHeader>
+								<DrawerTitle>
+									<h2 className="text-3xl font-bold tracking-tight">
+										Incident Details
+									</h2>
+								</DrawerTitle>
+							</DrawerHeader>
+							<DrawerDescription>
+								<Card>
+									<CardContent>
+										<img src={location.pictureLink} alt="Incident Image" />
+									</CardContent>
+								</Card>
+							</DrawerDescription>
+						</DrawerContent>
+					</Drawer>
+				</>
+			);
 		},
-	  },
-	  
+	},
 ];
